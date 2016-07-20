9f640380bf61f1503cbebb3576dc190a2c99cc8d
dimsuz <dimsuz@gmail.com>
2016-07-21 00:49:59 +0200

    Document a return value of Router.handleBack() (#93)

--- conductor/src/main/java/com/bluelinelabs/conductor/Router.java
+++ conductor/src/main/java/com/bluelinelabs/conductor/Router.java
@@ -71,6 +71,8 @@ public abstract class Router {
     /**
      * This should be called by the host Activity when its onBackPressed method is called. The call will be forwarded
      * to its top {@link Controller}. If that controller doesn't handle it, then it will be popped.
+     *
+     * @return Whether or not a back action was handled by the Router
      */
     public boolean handleBack() {
         if (!backStack.isEmpty()) {
