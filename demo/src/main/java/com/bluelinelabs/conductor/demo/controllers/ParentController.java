09ce640d9c01363cae0a86b67acf532ebd7de5c1
Eric Kuck <eric@bluelinelabs.com>
2016-12-14 19:42:10 -0600

    Fixes #183

--- demo/src/main/java/com/bluelinelabs/conductor/demo/controllers/ParentController.java
+++ demo/src/main/java/com/bluelinelabs/conductor/demo/controllers/ParentController.java
@@ -50,17 +50,19 @@ public class ParentController extends BaseController {
             childController.addLifecycleListener(new LifecycleListener() {
                 @Override
                 public void onChangeEnd(@NonNull Controller controller, @NonNull ControllerChangeHandler changeHandler, @NonNull ControllerChangeType changeType) {
-                    if (changeType == ControllerChangeType.PUSH_ENTER && !hasShownAll) {
-                        if (index < NUMBER_OF_CHILDREN - 1) {
-                            addChild(index + 1);
-                        } else {
-                            hasShownAll = true;
-                        }
-                    } else if (changeType == ControllerChangeType.POP_EXIT) {
-                        if (index > 0) {
-                            removeChild(index - 1);
-                        } else {
-                            getRouter().popController(ParentController.this);
+                    if (!isBeingDestroyed()) {
+                        if (changeType == ControllerChangeType.PUSH_ENTER && !hasShownAll) {
+                            if (index < NUMBER_OF_CHILDREN - 1) {
+                                addChild(index + 1);
+                            } else {
+                                hasShownAll = true;
+                            }
+                        } else if (changeType == ControllerChangeType.POP_EXIT) {
+                            if (index > 0) {
+                                removeChild(index - 1);
+                            } else {
+                                getRouter().popController(ParentController.this);
+                            }
                         }
                     }
                 }
