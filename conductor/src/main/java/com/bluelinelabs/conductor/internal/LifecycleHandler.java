victorlira/Conductor

commit e64fe1c61027db77a788c7a9abeaecac166a11ff

Fixes issue when immediately creating two routers in the same Activity caused by the async nature of fragment transactions (fixes #299)

conductor/src/main/java/com/bluelinelabs/conductor/internal/LifecycleHandler.java
@@ -41,6 +41,7 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
     private boolean destroyed;
     private boolean attached;
 
+    private static final Map<Activity, LifecycleHandler> activeLifecycleHandlers = new HashMap<>();
     private SparseArray<String> permissionRequestMap = new SparseArray<>();
     private SparseArray<String> activityRequestMap = new SparseArray<>();
     private ArrayList<PendingPermissionRequest> pendingPermissionRequests = new ArrayList<>();
@@ -54,7 +55,10 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
 
     @Nullable
     private static LifecycleHandler findInActivity(@NonNull Activity activity) {
-        LifecycleHandler lifecycleHandler = (LifecycleHandler)activity.getFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+        LifecycleHandler lifecycleHandler = activeLifecycleHandlers.get(activity);
+        if (lifecycleHandler == null) {
+            lifecycleHandler = (LifecycleHandler)activity.getFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+        }
         if (lifecycleHandler != null) {
             lifecycleHandler.registerActivityListener(activity);
         }
@@ -67,6 +71,11 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
         if (lifecycleHandler == null) {
             lifecycleHandler = new LifecycleHandler();
             activity.getFragmentManager().beginTransaction().add(lifecycleHandler, FRAGMENT_TAG).commit();
+
+            // Since Fragment transactions are async, we have to keep an <Activity, LifecycleHandler> map in addition
+            // to trying to find the LifecycleHandler fragment in the Activity to handle the case of the developer
+            // trying to immediately get > 1 router in the same Activity. See issue #299.
+            activeLifecycleHandlers.put(activity, lifecycleHandler);
         }
         lifecycleHandler.registerActivityListener(activity);
         return lifecycleHandler;
@@ -357,7 +366,9 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
     }
 
     @Override
-    public void onActivityDestroyed(Activity activity) { }
+    public void onActivityDestroyed(Activity activity) {
+        activeLifecycleHandlers.remove(activity);
+    }
 
     private static class PendingPermissionRequest implements Parcelable {
         final String instanceId;

commit c298ca905cf964b9dc8917ddad325660425112a6

Fixes leaked activity introduced in e64fe1c61027db77a788c7a9abeaecac166a11ff

conductor/src/main/java/com/bluelinelabs/conductor/internal/LifecycleHandler.java
@@ -71,11 +71,6 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
         if (lifecycleHandler == null) {
             lifecycleHandler = new LifecycleHandler();
             activity.getFragmentManager().beginTransaction().add(lifecycleHandler, FRAGMENT_TAG).commit();
-
-            // Since Fragment transactions are async, we have to keep an <Activity, LifecycleHandler> map in addition
-            // to trying to find the LifecycleHandler fragment in the Activity to handle the case of the developer
-            // trying to immediately get > 1 router in the same Activity. See issue #299.
-            activeLifecycleHandlers.put(activity, lifecycleHandler);
         }
         lifecycleHandler.registerActivityListener(activity);
         return lifecycleHandler;
@@ -122,6 +117,11 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
         if (!hasRegisteredCallbacks) {
             hasRegisteredCallbacks = true;
             activity.getApplication().registerActivityLifecycleCallbacks(this);
+
+            // Since Fragment transactions are async, we have to keep an <Activity, LifecycleHandler> map in addition
+            // to trying to find the LifecycleHandler fragment in the Activity to handle the case of the developer
+            // trying to immediately get > 1 router in the same Activity. See issue #299.
+            activeLifecycleHandlers.put(activity, this);
         }
     }
 
@@ -156,6 +156,7 @@ public class LifecycleHandler extends Fragment implements ActivityLifecycleCallb
 
         if (activity != null) {
             activity.getApplication().unregisterActivityLifecycleCallbacks(this);
+            activeLifecycleHandlers.remove(activity);
             destroyRouters();
             activity = null;
         }