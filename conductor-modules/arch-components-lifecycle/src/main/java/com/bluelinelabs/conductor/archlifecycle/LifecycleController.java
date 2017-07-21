422ca7a544f63c2ec47049d3998423a30952d022
Gustavo Pagani <luizgrp@gmail.com>
2017-07-21 17:58:20 +0100

    Expose parent constructor with Bundle as parameter (#328)
    * Expose constructor with Bundle as parameter
    * Add Nullable annotation to Bundle parameter

--- conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
+++ conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
@@ -2,12 +2,22 @@ package com.bluelinelabs.conductor.archlifecycle;
 
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 import com.bluelinelabs.conductor.Controller;
 
 public abstract class LifecycleController extends Controller implements LifecycleRegistryOwner {
 
     private final ControllerLifecycleRegistryOwner lifecycleRegistryOwner = new ControllerLifecycleRegistryOwner(this);
+
+    public LifecycleController() {
+        super();
+    }
+
+    public LifecycleController(@Nullable Bundle args) {
+        super(args);
+    }
 
     @Override
     public LifecycleRegistry getLifecycle() {
