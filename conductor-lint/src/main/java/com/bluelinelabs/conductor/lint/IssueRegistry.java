562ed171c3dec1120818ccdb65ba05df0b8d3bd2
Paul Woitaschek <woitaschek@posteo.de>
2018-08-02 09:18:56 +0200

    Specify the current api for the IssueRegistry. (#460)

--- conductor-lint/src/main/java/com/bluelinelabs/conductor/lint/IssueRegistry.java
+++ conductor-lint/src/main/java/com/bluelinelabs/conductor/lint/IssueRegistry.java
@@ -5,10 +5,19 @@ import com.android.tools.lint.detector.api.Issue;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.android.tools.lint.detector.api.ApiKt.CURRENT_API;
+
 public final class IssueRegistry extends com.android.tools.lint.client.api.IssueRegistry {
-    @Override public List<Issue> getIssues() {
+
+    @Override
+    public List<Issue> getIssues() {
         return Arrays.asList(
                 ControllerIssueDetector.ISSUE,
                 ControllerChangeHandlerIssueDetector.ISSUE);
+    }
+
+    @Override
+    public int getApi() {
+        return CURRENT_API;
     }
 }
