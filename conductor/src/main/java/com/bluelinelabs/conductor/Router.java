94c9121bb16f93b481954513a8e3905846829fb2
Eric Kuck <eric@bluelinelabs.com>
2019-04-06 14:58:54 -0400

    Fixed logic error that could result in undestroyed controllers

--- conductor/src/main/java/com/bluelinelabs/conductor/Router.java
+++ conductor/src/main/java/com/bluelinelabs/conductor/Router.java
@@ -496,10 +496,15 @@ public abstract class Router {
         for (RouterTransaction removedTransaction : transactionsToBeRemoved) {
 
             // Still need to ensure the controller isn't queued up to be removed later on.
+            boolean willBeRemoved = false;
             for (ChangeTransaction pendingTransaction : pendingControllerChanges) {
-                if (pendingTransaction.from != removedTransaction.controller) {
-                    removedTransaction.controller.destroy();
+                if (pendingTransaction.from == removedTransaction.controller) {
+                    willBeRemoved = true;
                 }
+            }
+
+            if (!willBeRemoved) {
+                removedTransaction.controller.destroy();
             }
         }
     }
