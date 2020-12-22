6820aa7d6a5a045165855603ef8ba873bde0ed49
Eric Kuck <eric@bluelinelabs.com>
2020-12-22 13:09:57 -0600

    Convert demo app to Kotlin w/ ViewBinding (#635)

--- demo/src/main/java/com/bluelinelabs/conductor/demo/ActionBarProvider.java
+++
@@ -1,7 +0,0 @@
-package com.bluelinelabs.conductor.demo;
-
-import androidx.appcompat.app.ActionBar;
-
-public interface ActionBarProvider {
-    ActionBar getSupportActionBar();
-}
--- demo/src/main/java/com/bluelinelabs/conductor/demo/util/AnimUtils.java
+++ demo/src/main/java/com/bluelinelabs/conductor/demo/util/AnimUtils.java
@@ -21,19 +21,16 @@ import android.animation.TimeInterpolator;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.util.ArrayMap;
-import android.util.FloatProperty;
-import android.util.IntProperty;
-import android.util.Property;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
-
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 import androidx.transition.Transition;
+
+import java.util.ArrayList;
 
 /**
  * Utility methods for working with animations.
@@ -77,109 +74,6 @@ public class AnimUtils {
             linear = new LinearInterpolator();
         }
         return linear;
-    }
-
-    /**
-     * Linear interpolate between a and b with parameter t.
-     */
-    public static float lerp(float a, float b, float t) {
-        return a + (b - a) * t;
-    }
-
-    /**
-     * A delegate for creating a {@link Property} of <code>int</code> type.
-     */
-    public static abstract class IntProp<T> {
-
-        public final String name;
-
-        public IntProp(String name) {
-            this.name = name;
-        }
-
-        public abstract void set(T object, int value);
-        public abstract int get(T object);
-    }
-
-    /**
-     * The animation framework has an optimization for <code>Properties</code> of type
-     * <code>int</code> but it was only made public in API24, so wrap the impl in our own type
-     * and conditionally create the appropriate type, delegating the implementation.
-     */
-    public static <T> Property<T, Integer> createIntProperty(final IntProp<T> impl) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            return new IntProperty<T>(impl.name) {
-                @Override
-                public Integer get(T object) {
-                    return impl.get(object);
-                }
-
-                @Override
-                public void setValue(T object, int value) {
-                    impl.set(object, value);
-                }
-            };
-        } else {
-            return new Property<T, Integer>(Integer.class, impl.name) {
-                @Override
-                public Integer get(T object) {
-                    return impl.get(object);
-                }
-
-                @Override
-                public void set(T object, Integer value) {
-                    impl.set(object, value);
-                }
-            };
-        }
-    }
-
-    /**
-     * A delegate for creating a {@link Property} of <code>float</code> type.
-     */
-    public static abstract class FloatProp<T> {
-
-        public final String name;
-
-        protected FloatProp(String name) {
-            this.name = name;
-        }
-
-        public abstract void set(T object, float value);
-        public abstract float get(T object);
-    }
-
-    /**
-     * The animation framework has an optimization for <code>Properties</code> of type
-     * <code>float</code> but it was only made public in API24, so wrap the impl in our own type
-     * and conditionally create the appropriate type, delegating the implementation.
-     */
-    public static <T> Property<T, Float> createFloatProperty(final FloatProp<T> impl) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            return new FloatProperty<T>(impl.name) {
-                @Override
-                public Float get(T object) {
-                    return impl.get(object);
-                }
-
-                @Override
-                public void setValue(T object, float value) {
-                    impl.set(object, value);
-                }
-            };
-        } else {
-            return new Property<T, Float>(Float.class, impl.name) {
-                @Override
-                public Float get(T object) {
-                    return impl.get(object);
-                }
-
-                @Override
-                public void set(T object, Float value) {
-                    impl.set(object, value);
-                }
-            };
-        }
     }
 
     /**
